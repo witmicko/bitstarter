d5ce6658a0d43081a2b2c72adb7eb9d33feca394
witmicko <mjogrodniczak@gmail.com>
2013-07-09 19:49:27 +0100

    added index.html

--- web.js
+++ web.js
@@ -3,10 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});//test
+});
+
+var fs = require('fs');
+var infile = "index.html";
+fs.readFileSync(infile);
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
