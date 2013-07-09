f3174da09bcb23f5584830ed0e421ad2cf2a1c6c
witmicko <mjogrodniczak@gmail.com>
2013-07-09 20:37:33 +0100

    displaying contents of index.html

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs);
+  response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 9000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-var fs = require('fs');
-fs.readFileSync('index.html');
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+
